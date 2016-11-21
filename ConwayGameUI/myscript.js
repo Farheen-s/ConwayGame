953dec9a5695d57c45e20729874c9f066a26b72e
Farheen <farheensultana2020@gmail.com>
2016-11-21 15:34:13 -0500

    add script functions for button click

--- ConwayGameUI/myscript.js
+++ ConwayGameUI/myscript.js
@@ -1,6 +1,6 @@
 ﻿var pageMode = 1;
 
-function inputdata() {
+function InputGridData() {
     pageMode = 1;
 
     if (!(!isNaN($('#rows').val()) && parseInt($('#rows').val()) > 0 && !isNaN($('#columns').val()) && parseInt($('#columns').val()) > 0))
@@ -14,5 +14,32 @@ function inputdata() {
 
 function CreateGrid(rows, columns)
 {
+    var table = document.getElementById("conwayGrid");
+    $(table).html('');
+    for (var i = 0; i < rows; i++) {
+        var row = table.insertRow(i);
+        for (var j = 0; j < columns; j++) {
+            var cell1 = row.insertCell(j);
+        }
+    }
+
+    $('#conwayGrid tr td').click(function () {
+        if (pageMode == 1)
+            $(this).toggleClass('cellColor');
+    });
+    $('#simulateConway').show();
+}
+
+function SimulateGrid()
+{
+    pageMode = 2;
+    $('#simulateConway').prop('disabled', true);
+    var grid = getGridInfo();
+    $('#conwayGrid tr td').removeClass('cellColor');
+    simulateNext(grid);
+}
+
+function simulateNext(grid) {
+
 
 }
