1e0de8636a297529c39eebb9836deb16c78aff4c
Farheen <farheensultana2020@gmail.com>
2016-11-21 14:21:28 -0500

    add script to UI

---
+++ ConwayGameUI/myscript.js
@@ -0,0 +1,18 @@
+﻿var pageMode = 1;
+
+function inputdata() {
+    pageMode = 1;
+
+    if (!(!isNaN($('#rows').val()) && parseInt($('#rows').val()) > 0 && !isNaN($('#columns').val()) && parseInt($('#columns').val()) > 0))
+        return;
+
+    var rows = parseInt($('#rows').val());
+    var columns = parseInt($('#columns').val());
+    CreateGrid(rows, columns);
+}
+
+
+function CreateGrid(rows, columns)
+{
+
+}
